893eff32230cf9a5ce71ba49d42e9b39a80fab5e
zhouplus <zhouplus@hotmail.com>
2022-05-13 16:57:26 +0800

    fix: use sqlx for db mappings.

--- crates/models/src/lib.rs
+++ crates/models/src/lib.rs
@@ -1,42 +1,8 @@
-use sea_orm::{ColumnTrait, Database, DatabaseConnection, EntityTrait, QueryFilter, QuerySelect};
-
 pub mod entity;
 
-pub async fn establish_connection() -> DatabaseConnection {
-    let database_url = "mysql://root:root@127.0.0.1:3306/mgateway";
-    let db = Database::connect(database_url)
-        .await
-        .expect(&format!("failed to connect to: {}", database_url));
-
-    return db;
-}
-
-pub async fn test(db: &DatabaseConnection) -> anyhow::Result<Option<entity::module::Model>> {
-    let ret = entity::module::Entity::find_by_id(1).one(db).await?;
-    let rr = entity::module::Entity::find()
-        .filter(entity::module::Column::ModuleName.like("%w%"))
-        .one(db)
-        .await?;
-    if let Some(mt) = rr {
-        println!("with www: {:#?}", mt);
-    }
-
-    let joret = entity::matcher::Entity::find()
-        .join(
-            sea_orm::JoinType::LeftJoin,
-            entity::matcher::Entity::belongs_to(entity::matcher_module::Entity)
-                .from(entity::matcher::Column::Id)
-                .to(entity::matcher_module::Column::MatcherId)
-                .into(),
-        )
-        .limit(10)
-        .filter(entity::matcher_module::Column::SortIndex.gt(2))
-        .all(db)
-        .await?;
-    println!("{:#?}", joret);
-
+pub async fn test() -> anyhow::Result<()> {
     // test for sql builder
-    Ok(ret)
+    Ok(())
 }
 
 pub fn enable_tracing() {}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-use std::{io::Write};
+use std::io::Write;
 
+use futures::StreamExt;
 use rbatis::{
-    crud::{CRUDTable, CRUD},
+    crud::{CRUD, CRUDTable},
     rbatis::Rbatis,
 };
 use serde::{Deserialize, Serialize};
+use sqlx::{Column, FromRow, Row};
 
 #[derive(Deserialize, Serialize, Debug)]
 struct MatcherModel {
@@ -20,23 +22,87 @@ struct MatcherModel {
     pub match_method: String,
 }
 
+#[derive(FromRow, Debug)]
+#[allow(dead_code)]
+struct MatcherModelSqlx {
+    pub id: i64,
+    pub match_type: i32,
+    pub match_value: String,
+    pub match_target: i32,
+    pub version: i32,
+    pub deleted_at: Option<chrono::NaiveDateTime>,
+    pub created_at: chrono::NaiveDateTime,
+    pub updated_at: chrono::NaiveDateTime,
+    pub upstream: String,
+    pub match_method: String,
+}
+
+#[derive(Debug)]
+#[allow(dead_code)]
+struct MatcherModelMacro {
+    pub id: i64,
+    pub match_type: i32,
+    pub match_value: String,
+    pub match_target: i32,
+    pub version: i32,
+    pub deleted_at: Option<sqlx::types::time::PrimitiveDateTime>,
+    pub created_at: sqlx::types::time::PrimitiveDateTime,
+    pub updated_at: sqlx::types::time::PrimitiveDateTime,
+    pub upstream: String,
+    pub match_method: String,
+}
+
 impl CRUDTable for MatcherModel {
-    fn table_columns() -> String {
-        return "id,match_type,match_value,match_target,version,deleted_at,created_at,updated_at,upstream,match_method".to_string();
-    }
     fn table_name() -> String {
         return "matcher".to_owned();
     }
+    fn table_columns() -> String {
+        return "id,match_type,match_value,match_target,version,deleted_at,created_at,updated_at,upstream,match_method".to_string();
+    }
 }
 
-#[actix_web::main]
-async fn main() -> anyhow::Result<()> {
+async fn test_for_sqlx() -> anyhow::Result<()> {
+    let pool = sqlx::MySqlPool::connect("mysql://root:root@192.168.150.73:3306/mgateway").await?;
+
+    log::info!("==================== run in low level ===================");
+
+    let ret = sqlx::query("SELECT * FROM matcher WHERE id > ? LIMIT 1").bind("123").fetch_one(&pool).await?;
+
+    for i in ret.columns() {
+        log::info!("{}", i.name());
+    }
+    let v = ret.get::<String, &str>("match_value");
+    log::info!("{}", v);
+
+    log::info!("=================== response mappings ====================");
+
+    let stream_ret = sqlx::query_as::<_, MatcherModelSqlx>("SELECT * FROM matcher WHERE match_value LIKE ? LIMIT 10").bind("%/v1.0%").fetch(&pool);
+
+    stream_ret.for_each(|v| {
+        if let Ok(mm) = v {
+            log::info!("{:#?}", mm);
+        }
+        futures::future::ready(())
+    }).await;
+
+    log::info!("==================== use macro to check validity and mapping ===================");
+
+    let matchers = sqlx::query_as!(MatcherModelMacro, "SELECT * FROM matcher WHERE match_value LIKE ? LIMIT ?", "%/v1.0%", 10).fetch_all(&pool).await?;
+
+    for m in matchers {
+        log::info!("{:#?}", m);
+    }
+
+    Ok(())
+}
+
+fn init_log() {
     env_logger::Builder::new()
         .format(|buf, record| {
             writeln!(
                 buf,
                 "{}:{} {} [{}] - {}",
-                record.file().unwrap_or("n"),
+                record.file().unwrap_or("0"),
                 record.line().unwrap_or(0),
                 chrono::Local::now().format("%Y-%m-%d %H:%M:%S.%3f"),
                 record.level(),
@@ -45,18 +111,19 @@ async fn main() -> anyhow::Result<()> {
         })
         .filter_level(log::LevelFilter::Debug)
         .init();
+}
+
+#[actix_web::main]
+async fn main() -> anyhow::Result<()> {
+    init_log();
 
     log::info!("connecting to database");
+    test_for_sqlx().await?;
 
-    // let connection = establish_connection().await;
-
-    // let ret = models::test(&connection).await?;
-    // if let Some(m) = ret {
-    //     log::info!("{:#?}", m);
-    // }
+    log::info!("==================== use rbatis for mapping ===================");
 
     let rb = Rbatis::new();
-    rb.link("mysql://root:root@127.0.0.1:3306/mgateway").await?;
+    rb.link("mysql://root:root@192.168.150.73:3306/mgateway").await?;
 
     let wrapper = rb
         .new_wrapper()
@@ -71,6 +138,9 @@ async fn main() -> anyhow::Result<()> {
         .gt("version", 1u32)
         .order_by(false, &["id"])
         .limit(10);
+
+    let ret = rb.fetch_list_by_wrapper::<MatcherModel>(wrapper).await?;
+    log::info!("{:#?}", ret);
 
     Ok(())
 }
